949cac06a9d8aa315b16bffd520e307c0382bc68
Linuxman <32411800+xlinuxmanx@users.noreply.github.com>
2019-05-19 17:24:28 -0500

    Update openvpn.sh

--- openvpn.sh
+++ openvpn.sh
@@ -20,12 +20,12 @@ sudo cp server.conf $OpenVPNPath/
 echo -e "\e[1m\e[32m[\e[1m\e[31m*\e[1m\e[32m] Creating Server Certificates\e[39m\e[0m"
 echo -e "\e[1m\e[32m[\e[1m\e[31m*\e[1m\e[32m] Generating CA\e[39m\e[0m"
 sudo openssl genrsa -out $OpenVPNPath/ca.key 2048
-sudo openssl req -new -x509 -days 1826 -key $OpenVPNPath/ca.key -out $OpenVPNPath/ca.crt -sha256 -subj "/C=./ST=./L=./O=./OU=./CN=$Host" -nodes
+sudo openssl req -x509 -new -nodes -key $OpenVPNPath/ca.key -sha256 -days 1826 -subj "/C=./ST=./L=./O=./OU=./CN=$Host" -out $OpenVPNPath/ca.crt
 
 echo -e "\e[1m\e[32m[\e[1m\e[31m*\e[1m\e[32m] Generating Certificate\e[39m\e[0m"
 sudo openssl genrsa -out $OpenVPNPath/server.key 2048
-sudo openssl req -new -key $OpenVPNPath/server.key -out $OpenVPNPath/server.csr -subj "/C=./ST=./L=/O=./OU=./CN=$Host"
-sudo openssl x509 -req -days 365 -in $OpenVPNPath/server.csr -CA $OpenVPNPath/ca.crt -CAkey $OpenVPNPath/ca.key -out $OpenVPNPath/server.crt -sha256
+sudo openssl req -new -sha256 -key $OpenVPNPath/server.key -subj "/C=./ST=./L=/O=./OU=./CN=$Host" -out $OpenVPNPath/server.csr
+sudo openssl x509 -req -in $OpenVPNPath/server.csr -CA $OpenVPNPath/ca.crt -CAkey $OpenVPNPath/ca.key -out $OpenVPNPath/server.crt -days 365 -sha256
 
 echo -e "\e[1m\e[32m[\e[1m\e[31m*\e[1m\e[32m] Generate Diffie-Hellman PEM\e[39m\e[0m"
 sudo openssl dhparam -out $OpenVPNPath/dh2048.pem 2048
