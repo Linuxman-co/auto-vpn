d149e8edb6339606fe5ec0a6b0ff3aecdbf357d0
Linuxman <32411800+xlinuxmanx@users.noreply.github.com>
2019-05-26 21:18:53 -0500

    Update win-client.sh

--- win-client.sh
+++ win-client.sh
@@ -10,14 +10,13 @@ create_client_config() {
 	ClientDir=/etc/openvpn/$1
 	ClientConf=/etc/openvpn/$1/$1.ovpn
 	PublicIP=$(curl -s icanhazip.com)
-	Host=$(hostname)
+	
 	echo -e "\e[1m\e[32m[\e[1m\e[31m*\e[1m\e[32m] Prepping Client Environment\e[0m\e[39m"
 	sudo mkdir $ClientDir
 
-	echo -e "\e[1m\e[32m[\e[1m\e[31m*\e[1m\e[32m] Generating Client Cetificate for $1\e[0m\e[39m"
-	sudo openssl genrsa -out $ClientDir/$1.key 2048
-	sudo openssl req -new -sha256 -key $ClientDir/$1.key -subj "/C=AA/ST=BB/L=CC/O=DD/OU=EE/CN=$Host" -out $ClientDir/$1.csr -nodes
-	sudo openssl x509 -req -days 365 -in $ClientDir/$1.csr -CA /etc/openvpn/ca.crt -CAkey /etc/openvpn/ca.key -set_serial 01 -out $ClientDir/$1.crt -sha256
+	echo -e "\e[1m\e[32m[\e[1m\e[31m*\e[1m\e[32m] Getting Client Cetificate for $1\e[0m\e[39m"
+	gzip -d /usr/share/doc/openvpn/examples/sample-keys/client.crt.gz
+	cp /usr/share/doc/openvpn/examples/sample-keys/{client.crt,client.key} $ClientDir
 
 	echo -e "\e[1m\e[32m[\e[1m\e[31m*\e[1m\e[32m] Creating Config\e[0m\e[39m"
 	echo client >> $ClientConf
